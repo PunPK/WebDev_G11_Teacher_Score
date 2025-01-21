3dfec3f71f4fe8f1e27792cfaf2cc559655232e6
6710110151 <chinochi866@gmail.com>
2025-01-21 22:27:34 +0700

    Separate path and fix issue

--- teacher-score-frontend/src/App.js
+++ teacher-score-frontend/src/App.js
@@ -1,16 +1,37 @@
+import {
+  Link,
+  Navigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import React, { useContext } from "react";
-
 import { AuthContext } from "./context/Auth.context.js";
 // import Login from "./Login/index.js";
 // import Dashboard from "./Dashboard/index.js";
 import LoginForm from "./page/login.js";
 import HomePage from "./page/home.js";
 
-const App = () => {
+function App() {
   const { state } = useContext(AuthContext);
 
-  if (!state.isLoggedIn) return <LoginForm />;
-  else return <HomePage />;
-};
+  if (!state.isLoggedIn) return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>);
+  else
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
+    );
+}
 
 export default App;
