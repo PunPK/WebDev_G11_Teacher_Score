c3f2c07b176ebf681d0b05a922d8a04eb2087023
6710110151 <chinochi866@gmail.com>
2025-01-23 23:02:04 +0700

    add home lecturer table update

--- teacher-score-frontend/src/lecturer/page/home.js
+++ teacher-score-frontend/src/lecturer/page/home.js
@@ -1,25 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-
+import { Pagination, Table } from "flowbite-react";
 import { AuthContext } from "../../context/Auth.context.js";
 import ax from "../../conf/ax.js";
 import { useNavigate } from "react-router";
 import InfoCard from '../../components/Cards/InfoCard'
 import RoundIcon from '../../components/RoundIcon'
 import response from '../../utils/demo/tableData'
-import {
-  TableBody,
-  TableContainer,
-  Table,
-  TableHeader,
-  TableCell,
-  TableRow,
-  TableFooter,
-  Avatar,
-  Badge,
-  Pagination,
-} from '@windmill/react-ui'
-import axios from "axios";
-import { BugAntIcon } from "@heroicons/react/16/solid";
+import { Badge, } from '@windmill/react-ui'
+import dayjs from "dayjs";
+import { Card, Typography } from "@material-tailwind/react";
 
 const HomeLecturer = () => {
   const [subjectData, setSubjectData] = useState([]);
@@ -27,6 +16,7 @@ const HomeLecturer = () => {
   const { state: ContextState, logout } = useContext(AuthContext);
   const [page, setPage] = useState(1)
   const [data, setData] = useState([])
+  const [totalTopic, setTotalTopic] = useState(0)
   const { user } = ContextState;
   const onLogout = (e) => {
     e.preventDefault();
@@ -35,7 +25,7 @@ const HomeLecturer = () => {
 
   const fetchSubject = async () => {
     try {
-      const subjectUrl = "http://localhost:1337/api/subjects"
+      const subjectUrl = "http://localhost:1337/api/subjects?populate=*"
       const response = await ax.get(subjectUrl)
       console.log(response.data.data)
       setSubjectData(response.data.data)
@@ -44,9 +34,10 @@ const HomeLecturer = () => {
     }
   };
 
+
   // pagination setup
-  const resultsPerPage = 20
-  const totalResults = response.length
+  const resultsPerPage = 10
+  const totalResults = subjectData.length
 
   // pagination change control
   function onPageChange(p) {
@@ -63,10 +54,12 @@ const HomeLecturer = () => {
     setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
   }, [page])
 
+  const TABLE_HEAD = ["รายชื่อวิชา", "จำนวนเรื่อง", "สถานะ", "วันที่สร้าง", "แก้ไขล่าสุด", "เข้ารายวิชา"]
+
   return (
-    <div className>
+    <div >
       <div className="col-sm-8 row-span-1">
-        <h1>Hello {user.email}</h1>
+        <h1>Hello lecturer {user.username}</h1>
       </div>
 
       <div className="col-sm-4">
@@ -77,90 +70,85 @@ const HomeLecturer = () => {
         </h1>
       </div>
 
-      <div class="grid gap-4">
-        <h1 class="mx-auto my-5 text-5xl font-sans">Anounced Score Subjects</h1>
+
+      <h1 class="mx-auto my-5 text-5xl font-sans">Anounced Score Subjects</h1>
 
 
-        {/* <!-- Cards --> */}
-        <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-          <InfoCard title="Total Subjects" value="6389">
-            {/* <RoundIcon
+      {/* <!-- Cards --> */}
+      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        <InfoCard title="Total Subjects" value={subjectData.length}>
+          {/* <RoundIcon
               // icon={ }
               iconColorClass="text-orange-500 dark:text-orange-100"
               bgColorClass="bg-orange-100 dark:bg-orange-500"
               className="mr-4"
             /> */}
-          </InfoCard>
-        </div>
-
-        <TableContainer className="bg-cyan-900">
-          <table >
-            <thead >
+        </InfoCard>
+      </div>
+      <div>
+        <Card className="h-full w-auto overflow-scroll">
+          <table className="w-auto min-w-8  text-left">
+            <thead>
               <tr >
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-lg font-semibold bg-cyan-900 text-white-100 uppercase tracking-wider"
-                >
-                  Subjects
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-lg font-semibold bg-cyan-900 text-white-100 uppercase tracking-wider"
-                >
-                  Total Topic
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-lg font-semibold bg-cyan-900 text-white-100 uppercase tracking-wider"
-                >
-                  Subject Status
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-lg font-semibold bg-cyan-900 text-white-100 uppercase tracking-wider"
-                >
-                  Open Topic
-                </th>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
               </tr>
             </thead>
-            <TableBody>
+            <tbody>
               {subjectData.map((user, i) => (
-                <TableRow key={i}>
-                  <TableCell>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <th sscope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     <div className="flex items-center text-sm">
                       <div>
                         <p className="font-semibold">{user.title}</p>
                         <p className="text-xs text-gray-600 dark:text-gray-400">{user.description}</p>
                       </div>
                     </div>
-                  </TableCell>
-                  <TableCell>
-                    <span className="text-sm">$ {user.create_date}</span>
-                  </TableCell>
-                  <TableCell>
-                    <p className="font-semibold">{user.title}</p>
+                  </th>
+                  <td class="px-6 py-4">
+                    <span className="text-sm">{user.topics.length === 0 ? <Typography class="text-red-600">ไม่มีหัวข้อ</Typography> : user.topics.length}</span>
+                  </td>
+                  <td class="px-6 py-4">
+                    <span className="text-sm">{dayjs(user.createdAt).format("DD/MM/YYYY - HH:mm")}</span>
+                  </td>
+                  <td class="px-6 py-4">
+                    <span className="text-sm">{dayjs(user.createdAt).format("DD/MM/YYYY - HH:mm")}</span>
+                  </td>
+                  <td class="px-6 py-4">
+                    <p >{dayjs(user.updatedAt).format("DD / MM / YYYY เวลา HH:mm น.")}</p>
                     {/* <Badge type={user.status}>{user.status}</Badge> */}
-                  </TableCell>
-                  <TableCell>
+                  </td>
+                  <td class="px-6 py-4">
                     {/* <span className="text-sm">{new Date(user.date).toLocaleDateString()}</span> */}
-                    <button>Open</button>
-                  </TableCell>
-                </TableRow>
+                    <button className="font-semibold bg-indigo-700 text-white-100 hover:bg-cyan-700 hover:text-cyan-50 w-14 h-8 justify-items-center">Open</button>
+                  </td>
+                </tr>
               ))}
-            </TableBody>
+            </tbody>
           </table>
-          <TableFooter >
-            <Pagination
-              totalResults={totalResults}
-              resultsPerPage={resultsPerPage}
-              label="Table navigation"
-              onChange={onPageChange}
-
-            />
-          </TableFooter>
-        </TableContainer>
+        </Card>
+      </div>
+      <div className="flex overflow-x-auto sm:justify-center">
+        {/* <Pagination
+            currentPage={page}
+            totalPages={totalResults}
+            onPageChange={onPageChange}
+          /> */}
       </div>
     </div>
+
 
   );
 };
