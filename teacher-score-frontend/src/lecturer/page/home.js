16192ccf08aa4a50afdcca0db39245627b5c8dfe
6710110151 <chinochi866@gmail.com>
2025-01-26 18:05:39 +0700

    add add subject button

--- teacher-score-frontend/src/lecturer/page/home.js
+++ teacher-score-frontend/src/lecturer/page/home.js
@@ -58,7 +58,7 @@ const HomeLecturer = () => {
   return (
     <>
       <Nav_lec />
-      <div class="grid bg-gradient-to-tl from-blue-800 to-cyan-300 min-h-screen max-h-screen top-0 mt-0 z-10">
+      <div class="grid bg-gradient-to-tr from-red-400 to-pink-500 min-h-screen max-h-full top-0 mt-0 z-10">
         <Card className="mt-16 mx-auto w-72 h-24 shadow-xl bg-white mb-4">
           <Typography className="items-center justify-items-center w-fit mx-auto my-auto">
             <h1 class="mx-auto text-5xl font-sans">รายการวิชา</h1>
@@ -78,53 +78,66 @@ const HomeLecturer = () => {
               </CardBody>
             </div>
           </Card>
+          <Card onClick={() => navigate("")} className="group bg-white hover:-translate-y-2 transition-all duration-200 delay-75 hover:drop-shadow-5xl cursor-pointer  hover:bg-gradient-to-tr hover:from-red-700 hover:to-pink-900  hover:shadow-red-400">
+            <div>
+              <CardBody >
+                <Typography className="font-bold text-lg group-hover:text-white">
+                  Add Subject
+                </Typography>
+              </CardBody>
+            </div>
+          </Card>
         </div>
-        <Card className="mx-28 h-auto bg-white my-2">
-          <div class=" grid grid-cols-3 gap-4 mx-6 my-5">
+        <Card className="mx-28 h-fit bg-white my-2 ">
+          <div class=" grid grid-cols-2 gap-4 mx-6 my-5">
             {subjectData.map((user) => (
               <>
-                { }
-                <Card
-                  onClick={() => navigate(`/topic/${user.documentId}`)}
-                  className="group h-full w-full  bg-gradient-to-tr from-blue-50 hover:-translate-y-2 transition-all duration-200 delay-75 hover:drop-shadow-5xl cursor-pointer  hover:bg-gradient-to-tr hover:from-blue-700 hover:to-blue-900  hover:shadow-blue-400 "
-                  style={{
-                    transformStyle: "preserve-3d",
-                  }}
-                >
-                  <CardBody>
-                    <Typography
-                      vatiant="h5"
-                      className="mb-2 text-2xl font-bold group-hover:text-white"
-                    >
-                      {user.title}
-                    </Typography>
+                <>
+                  <Card >
+                    <button onClick={() => navigate("/edit")}>Edit</button>
+                  </Card>
+                </>
+                <>
 
-                    <Typography className="group-hover:text-white">
-                      {user.description}
-                    </Typography>
+                  <Card
+                    onClick={() => navigate(`/topic/${user.documentId}`)}
+                    className="z-10 group h-full w-full  bg-gradient-to-tr from-blue-50 hover:-translate-y-2 transition-all duration-200 delay-75 hover:drop-shadow-5xl cursor-pointer  hover:bg-gradient-to-tr hover:from-red-700 hover:to-pink-900  hover:shadow-red-400 "
+                  >
+                    <CardBody>
+                      <Typography
+                        vatiant="h5"
+                        className="mb-2 text-2xl font-bold group-hover:text-white"
+                      >
+                        {user.title}
+                      </Typography>
 
-                    <Typography className="group-hover:text-white">
-                      จำนวนเรื่อง :{" "}
-                      {user.topics.lenght === 0
-                        ? "ไม่มีหัวข้อ"
-                        : user.topics.lenght}
-                    </Typography>
+                      <Typography className="group-hover:text-white">
+                        {user.description}
+                      </Typography>
 
-                    <Typography className="group-hover:text-white">
-                      สร้างเมื่อ{" "}
-                      {dayjs(user.createdAt).format(
-                        "DD / MM / YYYY เวลา HH:mm น."
-                      )}
-                    </Typography>
+                      <Typography className="group-hover:text-white">
+                        จำนวนเรื่อง :{" "}
+                        {user.topics.lenght === 0
+                          ? "ไม่มีหัวข้อ"
+                          : user.topics.lenght}
+                      </Typography>
 
-                    <Typography className="group-hover:text-white">
-                      อัพเดพล่าสุด{" "}
-                      {dayjs(user.updatedAt).format(
-                        "DD / MM / YYYY เวลา HH:mm น."
-                      )}
-                    </Typography>
-                  </CardBody>
-                </Card>
+                      <Typography className="group-hover:text-white">
+                        สร้างเมื่อ{" "}
+                        {dayjs(user.createdAt).format(
+                          "DD / MM / YYYY เวลา HH:mm น."
+                        )}
+                      </Typography>
+
+                      <Typography className="group-hover:text-white">
+                        อัพเดพล่าสุด{" "}
+                        {dayjs(user.updatedAt).format(
+                          "DD / MM / YYYY เวลา HH:mm น."
+                        )}
+                      </Typography>
+                    </CardBody>
+                  </Card>
+                </>
               </>
             ))}
           </div>
